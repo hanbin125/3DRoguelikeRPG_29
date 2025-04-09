241303b79d6215b89e275263454f0280510f8283
hanbin125 <cyhm2545@naver.com>
2025-04-09 20:09:41 +0900

    [Feat] 카메라따라가게하기추가 및 수치변경할 베이스완료

--- Assets/Scripts/Player/Test.cs
+++ Assets/Scripts/Player/Test.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     [SerializeField] Player _player;
-
-    public void Damage()
+    public void MaxHPUp()
     {
-        _player.TakeDamage(40);
+        _player.MaxHPUp(100);
     }
     public void Healing()
     {
         _player.Healing(40);
     }
+
+    public void MaxMPUp()
+    {
+        _player.MaxMPUp(100);
+    }
+
+    public void BaseMPUp()
+    {
+        _player.BaseMPUp(20);
+    }
     public void SpeedUp()
     {
         _player.SpeedUp(10);
+    }
+    public void AttackUp()
+    {
+        _player.AttackUp(10);
+    }
+
+    public void DMGReductionUp()
+    {
+        _player.DMGReductionUp(0.1f);
+    }
+    public void CriticalChanceUp()
+    {
+        _player.CriticalChanceUp(5);
+    }
+    public void CriticalDamageUp()
+    {
+        _player.CriticalDamageUp(0.25f);
+    }
+    public void Damage()
+    {
+        _player.TakeDamage(40);
     }
 }
